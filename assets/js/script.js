1504bdaa2365165b8cbbb47ead2a770b1d8eb6bb
Jorge Molina <jorge.e.molina@hotmail.com>
2021-12-09 18:09:05 -0600

    added the prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,28 @@
 // Assignment code here
-
+var enter;
+var cofirmNumber;
+var confirmUppsercase;var confirmLowerCase;
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 function generatePassword () {
   console.log("Hey you click a button!");
 //prompt user for password criteria
 //prompt for lenght between 8 and 128
+enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+
+if (!enter) {
+  alert("This needs a value");
+} else if (enter <8 || enter > 128) {
+  // validate input
+  enter = parseInt(prompt("You must choose between 8 and 128"));
+
+} else {
+  confirmNumber = confirm("Will this contain numbers?");
+  confirmCharacter = confirm("Will this contain Special characters?");
+  confirmUppercase = confirm("Will this contain Lowercase characters?");
+}
 //prompt for lowercase, uppercase and special charaters
-// validate input 
+ 
 //display generated password.
 
 
